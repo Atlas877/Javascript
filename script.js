a6b143b940524dfa8e190d2e1f4bb58d7596f331
Jaime <160668786+Atlas877@users.noreply.github.com>
2024-05-04 00:57:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var pessoa = {nome:'James',idade:27};
+
+class Pessoa {
+
+    constructor (nome,idade,endereco) {
+
+        this.nome = nome
+        this.idade = idade
+        this.endereco = endereco
+    }
+}
+
+
+var Pessoa1 = new Pessoa ('james',27,'Rua Almirante Reis');
