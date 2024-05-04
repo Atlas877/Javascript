c09dd4f4e9c992d8b89b5887585628473dfc9c6a
Jaime <160668786+Atlas877@users.noreply.github.com>
2024-05-04 01:04:07 +0100

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,21 @@
+
+
+// Esta variável é do tipo string
+var nomeCompleto = 'Jaime Manguele';
+
+// Esta varável é do tipo number
+var idade = '21';
+
+// Esta varável é do tipo null
+var dataNascimento;
+
+
+
+const pi = 3.14;
+
+const code = 'Trusted';
+
+
+let nome = 'Jaime';
+
+var idade = '20';
