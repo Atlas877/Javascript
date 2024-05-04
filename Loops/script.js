06816b8205b7112cd9128feaa058f637eb90584e
Jaime <160668786+Atlas877@users.noreply.github.com>
2024-05-04 01:02:09 +0100

    Add files via upload

---
+++ Loops/script.js
@@ -0,0 +1,22 @@
+
+function countNumbers(numeroMax){
+
+  /*  for (i=1;i<=100;i++){
+
+        console.log(i);
+    }
+
+  */
+
+   
+    var i = 1;
+
+    while(i <=numeroMax) {
+
+        console.log(i);
+        i=i+1;
+
+    }
+
+
+}
